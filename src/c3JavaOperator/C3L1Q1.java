ee4b6836e5c7a8d322ed63682da9ed1242c53a04
NimalAthith <tnimalathith@gmail.com>
2021-10-23 19:59:51 +0530

    Exercise

---
+++ src/c3JavaOperator/C3L1Q1.java
@@ -0,0 +1,15 @@
+package c3JavaOperator;
+/*
+ * Length and breadth of a rectangle are 5 and 7 respectively. Write a program to calculate the area and perimeter of the rectangle.
+ * 
+ */
+public class C3L1Q1 {
+	public static void main (String[] args) {
+		int length = 5;
+		int breadth = 7;
+		
+		System.out.println("Area of rectangle = " + (length * breadth) + (", Perimeter = " + 2*(length + breadth)));
+		
+	}
+
+}
